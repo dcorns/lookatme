9e993bb804adcaa9a416228ca7792a48369a2ca4
Dale Corns <drcorns@hotmail.com>
2014-05-16 15:30:32 -0700

    Refactor of server.js complete

--- api/database.js
+++ api/database.js
@@ -3,32 +3,110 @@
  */
 'use strict'
 var MongoClient = require('mongodb').MongoClient;//npm install mongodb
-//var Server = require('mongodb').Server; //npm install mongodb
+var User = require('./models/user');
 var BSON = require('mongodb').BSONPure; //npm install mongodb
-//var assert = require('assert');
-var dataBase = (function (){
 
-  //Configure mongo connection
-//  var mongoclient = new MongoClient(new Server('localhost', 27017, {native_parser: true}));
-//Configure mongo database
- // var db = mongoclient.db('onlineResumeDev');
+var dataBase = (function (){
 
   return{
     getAllUsers:function(req, res){
-      console.log('Running all users...');
+      console.log('Running getAllUsers...');
       MongoClient.connect('mongodb://localhost:27017/onlineResumeDev', function(err,db){
         if(err) throw err;
         db.collection('users').find({}).toArray(function(err, docs){
           if (err) res.send(err);
           res.json(docs);
           console.dir(docs);
-          db.close(function(){console.log('allUsers end');});
+          db.close(function(){console.log('getAllUsers end');});
         });
       });
     },
-    close: function(){
-      console.log('Closing connection to Database...')
-      return db.close();
+    createUser:function(req, res){
+      console.log('Running createUser...');
+      var user = new User();
+      user.firstName = req.body.firstName;
+      user.middleName = req.body.middleName;
+      user.lastName = req.body.lastName;
+      user.email = req.body.email;
+      user.mobilePhone = req.body.mobilePhone;
+      user.homePhone = req.body.homePhone;
+      user.workPhone = req.body.workPhone;
+      user.objective = req.body.objective;
+      user.userLinks = req.body.userLinks;
+      user.skills = req.body.skills;
+      user.education = req.body.education;
+      user.employmentHistory = req.body.employmentHistory;
+      user.interests = req.body.interests;
+      user.publications = req.body.publications;
+      user.certifications = req.body.certifications;
+      user.awards = req.body.awards;
+      user.references = req.body.references;
+      MongoClient.connect('mongodb://localhost:27017/onlineResumeDev', function(err,db){
+        if(err) throw err;
+        db.collection('users').insert(user,function(err, inserted) {
+          if (err)
+            res.send(err);
+
+          res.json({
+            message: 'User created!'
+          });
+          db.close(function(){console.log('createUser end');});
+        });
+      });
+    },
+    getUserById:function(req, res){
+      console.log('Running getUserById...');
+      MongoClient.connect('mongodb://localhost:27017/onlineResumeDev', function(err,db){
+        if(err) throw err;
+        var query = BSON.ObjectID.createFromHexString(req.params.id);
+        db.collection('users').findOne({_id: query}, function(err, doc){
+          if (err) res.send(err);
+          res.json(doc);
+          console.dir(doc); //remove after testing
+          db.close(function(){console.log('getUserById end');});
+        });
+      });
+    },
+    updateUserByID:function(req, res){
+      console.log('Running updateUserById...');
+      MongoClient.connect('mongodb://localhost:27017/onlineResumeDev', function(err,db){
+        if(err) throw err;
+        var id = BSON.ObjectID.createFromHexString(req.params.id);
+        var query = {'_id': id};
+        db.collection('users').findOne(query, function(err, doc) {
+          if (err)
+            res.send(err);
+
+          query._id = doc._id;
+          //put request objects are found in res.query
+          doc.lastName = req.query.lastName;
+          db.collection('users').update(query, doc, function(err, updated) {
+            if (err)
+              res.send(err);
+            res.json({
+              message: 'User updated!'
+            });
+          });
+          db.close(function(){console.log('updateUserById end');});
+        });
+      });
+    },
+    deleteUserById:function(req, res){
+      console.log('Running deleteUserById...');
+      MongoClient.connect('mongodb://localhost:27017/onlineResumeDev', function(err,db){
+        if(err) throw err;
+        var id = BSON.ObjectID.createFromHexString(req.params.id);
+        db.collection('users').remove({
+          _id: id
+        }, function(err, user) {
+          if (err)
+            res.send(err);
+          res.json({
+            message: 'User deleted!'
+          });
+          db.close(function(){console.log('deleteUserById end');});
+        });
+      });
     }
   };
 })();
--- server.js
+++ server.js
@@ -4,20 +4,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser'); //for getting data from a post
-var User = require('./api/models/user');
 var UserRoutes = require('./api/routes/userroutes.js');
 var dataBase = require('./api/database');
 var handlebars = require('handlebars');
 var cons = require('consolidate');
-var MongoClient = require('mongodb').MongoClient;//npm install mongodb
-var Server = require('mongodb').Server; //npm install mongodb
-var BSON = require('mongodb').BSONPure; //npm install mongodb
-//var format = require('util').format; //used for mongo db
-
-//Configure mongo connection
-var mongoclient = new MongoClient(new Server('localhost', 27017, {native_parser: true}));
-//Configure mongo database
-var db = mongoclient.db('onlineResumeDev');
 
 app.use(bodyParser());
 
@@ -42,35 +32,7 @@ router.use(function(req, res, next) {
 //On routes that end in /users
 router.route('/users')
 //create a user accessed at /api/users
-.post(function(req, res) {
-    var user = new User();
-    user.firstName = req.body.firstName;
-    user.middleName = req.body.middleName;
-    user.lastName = req.body.lastName;
-    user.email = req.body.email;
-    user.mobilePhone = req.body.mobilePhone;
-    user.homePhone = req.body.homePhone;
-    user.workPhone = req.body.workPhone;
-    user.objective = req.body.objective;
-    user.userLinks = req.body.userLinks;
-    user.skills = req.body.skills;
-    user.education = req.body.education;
-    user.employmentHistory = req.body.employmentHistory;
-    user.interests = req.body.interests;
-    user.publications = req.body.publications;
-    user.certifications = req.body.certifications;
-    user.awards = req.body.awards;
-    user.references = req.body.references;
-console.dir(user);
-    db.collection('users').insert(user,function(err, inserted) {
-        if (err)
-            res.send(err);
-
-        res.json({
-            message: 'User created!'
-        });
-    });
-})
+.post(function(req, res){dataBase.createUser(req, res);})
 
 //get all users accessed at /api/users
 .get(function(req, res){dataBase.getAllUsers(req, res);});
@@ -78,78 +40,18 @@ console.dir(user);
 //On routes that end in /api/users/:user_id
 //Note /:param1/:param2/:param3...(req.params.param1 etc.) and ?getvar1=value&getvar2=value&getvar3=value...(req.query.getvar1 etc.)
 router.route('/users/:id')
-//Find user by id
-.get(function(req, res) {
-      //Convert string id to ObjectID
-      var query = BSON.ObjectID.createFromHexString(req.params.id);
-
-      db.collection('users').findOne({_id: query}, function(err, doc){
-        if (err) res.send(err);
-        res.json(doc);
-        console.dir(doc); //remove after testing
-      });
-})
-
-.put(function(req, res) {
-    var id = BSON.ObjectID.createFromHexString(req.params.id);
-    var query = {'_id': id};
-    db.collection('users').findOne(query, function(err, doc) {
-        if (err)
-            res.send(err);
-
-      query._id = doc._id;
-      //put request objects are found in res.query
-      doc.lastName = req.query.lastName;
-      db.collection('users').update(query, doc, function(err, updated) {
-            if (err)
-                res.send(err);
-            res.json({
-                message: 'User updated!'
-            });
-        });
-    });
-})
-
-.delete(function(req, res) {
-    var id = BSON.ObjectID.createFromHexString(req.params.id);
-    db.collection('users').remove({
-        _id: id
-    }, function(err, user) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'User deleted!'
-        });
-
-    });
-});
+//Find user by _id
+.get(function(req,res){dataBase.getUserById(req,res);})
+//Update user by _id
+.put(function(req, res){dataBase.updateUserByID(req,res);})
+//Delete user by _id
+.delete(function(req, res){dataBase.deleteUserById(req,res);});
 
 app.use('/api', router); //prefix every route with /api
 //set up views
 
 // display landing page
 app.get('/',UserRoutes.landing);
- /*function(req, res) {
-    // render the page
-  //Called after connection to mongodb succeeds, it finds the first valid document in the users collection
-  db.collection('users').findOne({}, function(err,doc){
-    res.render('index', doc); //Any handlebar fields on index matching doc field names loaded here
-  });*/
- // res.render('index', {lastName : 'Dale Corns'});
-    /*res.render('index', {
-        partials: {
-            'lastName': 'user',
-        },
-        subTitle: 'Login',
-    });*/
-//});
 
 app.listen(port);
 console.log('Listening on port ' + port);
-//connect to the mongo db and don't run the web server unless the connection to the db is successful
-/*mongoclient.open(function (err, mongoclient){
-  if(err) throw err;
-  app.listen(port);
-  console.log('Listening on port' + port);*/
-
-//})
