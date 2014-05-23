778c167ea39053ae74944fe000e6c0c45f458ca1
Dale Corns <drcorns@hotmail.com>
2014-05-22 21:12:52 -0700

    Add UserViews.js

---
+++ app/assets/js/views/UserViews.js
@@ -0,0 +1,31 @@
+/**
+ * Created by dcorns on 5/22/14.
+ */
+
+var Backbone = require('backbone');
+var $ = require('jquery');
+var UserView = require('../js/views/UserView');
+Backbone.$ = $;
+
+module.exports = Backbone.View.extend({
+  className: 'notes',
+
+  initialize: function() {
+    this.collection.on('add', this.addUser, this);
+    this.collection.on('reset', this.addAll, this);
+  },
+
+  addUser: function(user) {
+    var userView = new UserView({model: user});
+    this.$el.append(userView.el);
+  },
+
+  addAll: function() {
+    this.collection.forEach(this.addUser);
+  },
+
+  render: function() {
+    this.addAll();
+  }
+
+});
